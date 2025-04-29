194d705b3aa4ffa09eb415ac486a59e5a5d74441
Gideongithub123 <gideoncrist98@gmail.com>
2025-04-29 21:17:00 +0800

    add package validasi dan tambah class validator

--- src/validasi/validator.java
+++ src/validasi/validator.java
@@ -1,4 +1,9 @@
 package validasi;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
 public class validator {
-}
+    private static Scanner scanner = new Scanner(System.in);
