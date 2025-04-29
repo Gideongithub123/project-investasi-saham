eae0553bbb62477d1200b9bc7f0826be936bec73
dippyyyy <devriaja24@gmail.com>
2025-04-29 15:13:19 +0800

    membuat class saham

--- src/model/Saham.java
+++ src/model/Saham.java
@@ -1,4 +1,12 @@
 package model;
 
 public class Saham {
-}
+    private String kode;               // Kode unik saham
+    private String namaPerusahaan;     // Nama perusahaan penerbit saham
+    private double harga;              // Harga per lembar saham
+
+    public Saham(String kode, String namaPerusahaan, double harga) {
+        this.kode = kode;
+        this.namaPerusahaan = namaPerusahaan;
+        this.harga = harga;
+    }
