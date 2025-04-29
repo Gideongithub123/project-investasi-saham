aa6dd7a187cadfc6efca5b4d9db8e50643495259
dippyyyy <devriaja24@gmail.com>
2025-04-29 15:16:12 +0800

    menambahkan getter dan setter, lalu membuat kode untuk menampilkan informasi saham

--- src/model/Saham.java
+++ src/model/Saham.java
@@ -10,3 +10,16 @@ public class Saham {
         this.namaPerusahaan = namaPerusahaan;
         this.harga = harga;
     }
+
+    // Getter dan setter
+    public String getKode() { return kode; }
+    public String getNamaPerusahaan() { return namaPerusahaan; }
+    public double getHarga() { return harga; }
+    public void setHarga(double harga) { this.harga = harga; }
+
+    // Menampilkan informasi saham
+    @Override
+    public String toString() {
+        return kode + " - " + namaPerusahaan + " @ Rp" + harga;
+    }
+}
