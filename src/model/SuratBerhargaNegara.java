bf427ceffd3acbcc178c947bc2e2d9470065599c
dippyyyy <devriaja24@gmail.com>
2025-04-29 20:06:27 +0800

    membuat kelas untuk sbn dan variabel yang digunakan

--- src/model/SuratBerhargaNegara.java
+++ src/model/SuratBerhargaNegara.java
@@ -1,4 +1,17 @@
 package model;
 
+// Kelas untuk SBN
 public class SuratBerhargaNegara {
-}
+    private String nama;                 // Nama produk SBN
+    private double bunga;               // Persentase bunga per tahun
+    private int jangkaWaktu;            // Dalam hitungan tahun
+    private String tanggalJatuhTempo;   // Tanggal jatuh tempo
+    private double kuotaNasional;       // Batas pembelian secara nasional
+
+    public SuratBerhargaNegara(String nama, double bunga, int jangkaWaktu, String tanggalJatuhTempo, double kuotaNasional) {
+        this.nama = nama;
+        this.bunga = bunga;
+        this.jangkaWaktu = jangkaWaktu;
+        this.tanggalJatuhTempo = tanggalJatuhTempo;
+        this.kuotaNasional = kuotaNasional;
+    }
